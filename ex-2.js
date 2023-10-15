a0835de45832fff9d3afe20ebe28dd0b5d829e01
Ploy <Ploypailin_mt@hotmail.com>
2023-10-15 18:32:03 +0700

    feat: finished ex-2

--- ex-2.js
+++ ex-2.js
@@ -12,7 +12,28 @@ const employees = [
   },
 ];
 // Start coding here
-let alex;
-let alexHobbies;
-let alexAndJamesAge;
-const updateEmployees = employees;
+let alex = employees[1].name;
+let alexHobbies = employees[1].hobbies;
+let alexAndJamesAge = employees[0].age + employees[1].age;
+alexHobbies.push("Watching basketball")
+
+const updateEmployees = [ employees,
+
+  {
+    name: "Alicia",
+    age: 29,
+    hobbies: ["Shopping","Reading novels"],
+  },
+  {
+    name: "Kody",
+    age: 19,
+    hobbies: ["Computer games","Wakeboard"],
+  }
+];
+console.log(updateEmployees);
+
+updateEmployees.pop();
+console.log(employees);
+console.log(updateEmployees);
+
+
